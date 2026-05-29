ee31356ddc6c302590b8d415645bb4bfb2828b7d
KOUSHIK C M <koushikmanohar83@gmail.com>
2026-05-29 09:21:32 +0530

    Create 02.global_constants.sv

---
+++ CLASS/05.Constant CLass Properties/02.global_constants.sv
@@ -0,0 +1,49 @@
+class aadhar;
+  
+  //declaring const in properties declaration
+  
+  const string name="RANGAJI";
+  const string dob="29/01/2004";
+  
+  function void display();
+    
+    //name and dob is not iniatized 
+    
+    $display("NAME and  DOB in aadhar");
+    $display("NAME:%S",name);
+
+    
+  endfunction
+  
+  
+endclass
+
+
+class e_sevai extends aadhar;
+  
+  function void display();
+    super.display();
+    $display("DOB:%S",dob);
+  endfunction
+    
+endclass
+
+
+module instance_constant;
+  e_sevai e;
+  
+  initial begin
+    e=new();
+    e.display;
+  end
+  
+endmodule
+    
+  
+  /*
+OUTPUT:
+NAME and  DOB in aadhar
+NAME:RANGAJI
+DOB:29/01/2004
+
+*/
