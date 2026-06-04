caf608080134824a9a88292784da2ae90be945af
KOUSHIK C M <koushikmanohar83@gmail.com>
2026-06-04 20:32:21 +0530

    Update 01.static_function.sv

--- FUNCTION/01.static_function/01.static_function.sv
+++ FUNCTION/01.static_function/01.static_function.sv
@@ -16,3 +16,38 @@ module test;
   end
 
 endmodule
+
+/*
+output:
+a=9 b=1
+SUM=10
+
+a=4 b=11
+SUM=15
+
+a=8 b=12
+SUM=20
+
+a=7 b=4
+SUM=11
+
+a=3 b=8
+SUM=11
+
+a=2 b=11
+SUM=13
+
+a=9 b=6
+SUM=15
+
+a=8 b=11
+SUM=19
+
+a=5 b=10
+SUM=15
+
+a=3 b=7
+SUM=10
+
+           V C S   S i m u l a t i o n   R e p o r t 
+*/
