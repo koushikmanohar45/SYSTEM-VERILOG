19cf8cfea6587cb54bbaaaca88ff166be5f86cad
KOUSHIK C M <koushikmanohar83@gmail.com>
2026-06-08 16:52:22 +0530

    Create 03.passing_variables_as_arguments_in_display.sv

---
+++ FUNCTION/03.Function Calling Methods/03.passing_variables_as_arguments_in_display.sv
@@ -0,0 +1,70 @@
+class alu;
+  
+  rand bit a,b;
+  function int operation
+  (
+    input int a,
+    input int b,
+    input string opcode
+  );
+    $display("a=%0d b=%0d",a,b);
+
+    case(opcode)
+
+      "ADD":return a+b;
+      "SUB":return a-b;
+      "MUL":return a*b;
+      "DIV":begin
+                if(b==0) begin
+                  $display("DIVIDE BY ZERO ERROR");
+                  return 0;
+                end
+                else
+                  return a/b;
+              end
+      default:begin
+                  $display("INVALID OPCODE");
+                  return -1;
+                end
+    endcase
+  endfunction
+endclass
+
+
+
+module test;
+
+  alu a1;
+
+  int result;
+
+  initial begin
+    
+    a1=new();
+    a1.randomize();
+    // Calling function with arguments from an expression
+    $display("ADD RESULT=%0d",a1.operation(a1.a,a1.b,"ADD"));
+
+    $display("SUB RESULT=%0d",a1.operation(a1.a,a1.b,"SUB"));
+
+    $display("MUL RESULT=%0d",a1.operation(a1.a,a1.b,"MUL"));
+
+    $display("DIV RESULT=%0d",a1.operation(a1.a,a1.b,"DIV"));
+    
+  end
+
+endmodule
+
+/*
+output:
+a=1 b=0
+ADD RESULT=1
+a=1 b=0
+SUB RESULT=1
+a=1 b=0
+MUL RESULT=0
+a=1 b=0
+DIVIDE BY ZERO ERROR
+DIV RESULT=0
+           V C S   S i m u l a t i o n   R e p o r t
+*/
